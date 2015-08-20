2da7bf87f63cc899fcbf5f3e8594e93f29982ba7
kriikas <kri05@hotmail.com>
2015-08-21 00:19:59 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -55,7 +55,6 @@ exports.new = function (req, res) {
 
 exports.create = function (req, res) {
   var quiz = models.Quiz.build(req.body.quiz);
-
   quiz
   .validate()
   .then(
@@ -69,6 +68,31 @@ exports.create = function (req, res) {
         .save({fields: ["pregunta", "respuesta"]})
         .then( function () { res.redirect('/quizes')})
       } //res.redirect(): Redireccion HTTP (URL relativo) lista de preguntas
+    }
+  );
+};
+
+//GET /quizes/:id/edit
+exports.edit = function (req, res) {
+  var quiz = req.quiz;
+  res.render('quizes/edit', {quiz: quiz});
+};
+
+//PUT /quizes/:id
+exports.update =function (req, res) {
+  req.quiz.pregunta = req.body.quiz.pregunta;
+  req.quiz.respuesta = req.body.quiz.respuesta;
+  req.quiz
+  .validate()
+  .then(
+    function (err) {
+      if(err){
+        res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+      }else{
+        req.quiz
+        .save( {fields: ["pregunta", "respuesta"]})
+        .then( function () { res.redirect('/quizes');});
+      }
     }
   );
 };
